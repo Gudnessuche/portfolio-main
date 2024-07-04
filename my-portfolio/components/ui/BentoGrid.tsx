1788122dddd6d9ce71804dfdc0c28260a898ec09
E S <everythingsatoshi1@gmail.com>
2024-07-04 21:35:23 +0000

    first commit

---
+++ my-portfolio/components/ui/BentoGrid.tsx
@@ -0,0 +1,169 @@
+'use client'
+
+import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./Globe";
+import { useState } from "react";
+import { IoCopyOutline } from "react-icons/io5";
+import { GlobeDemo } from "./GridGlobe";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+import animationData from "@/data/confetti.json";
+import Lottie from "react-lottie";
+import MagicButton from "./MagicButton";
+
+export const BentoGrid = ({
+    className,
+    children,
+}: {
+    className?: string;
+    children?: React.ReactNode;
+}) => {
+    return (
+        <div
+            className={cn(
+                "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+                className
+            )}
+        >
+            {children}
+        </div>
+    );
+};
+
+export const BentoGridItem = ({
+    className,
+    title,
+    description,
+    id,
+    img,
+    imgClassName,
+    titleclassName,
+    spareImg,
+
+}: {
+    className?: string;
+    title?: string | React.ReactNode;
+    description?: string | React.ReactNode;
+    header?: React.ReactNode;
+    icon?: React.ReactNode;
+    id?: number;
+    img?: string;
+    imgClassName?: string;
+    titleclassName?: string;
+    spareImg?: string;
+}) => {
+
+    const leftLists = ["ReactJS", "Express", "Typescript"];
+    const rightLists = ["Python", "NextJS", "MongoDB"];
+
+    const [copied, setCopied] = useState(false);
+
+    const defaultOptions = {
+        loop: copied,
+        autoplay: copied,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+
+    const handleCopy = () => {
+        const text = "everythingsatoshi1@gmail.com";
+        navigator.clipboard.writeText(text);
+        setCopied(true);
+    };
+
+    return (
+        <div
+            className={cn(
+                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
+                className
+            )}
+            style={{
+                background: 'rgb(4,7,29)',
+                backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
+            }}
+        >
+
+            <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+                <div className="w-full h-full absolute">
+                    {img && (
+                        <img
+                            src={img}
+                            alt={img}
+                            className={cn(imgClassName, 'object-cover, object-center')}
+                        />
+                    )}
+                </div>
+
+                <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+                    {spareImg && (
+                        <img
+                            src={spareImg}
+                            alt={spareImg}
+                            className={'object-cover, object-center w-full h-full'}
+                        />
+                    )}
+                </div>
+                {id === 6 && (
+                    <BackgroundGradientAnimation>
+                        {/* <div className="absolute z-50 flex items-center justify-center text-white font-bold" /> */}
+                    </BackgroundGradientAnimation>
+                )}
+
+                <div className={cn(
+                    titleclassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full flex flex-col px-5 p-5 lg:p-10'
+                )}>
+                    <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+                        {description}
+                    </div>
+                    <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                        {title}
+                    </div>
+
+
+                    {id === 2 && <GlobeDemo />}
+
+                    {id === 3 && (
+                        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                            <div className="flex flex-col gap-1 md:gap-1 lg:gap-5">
+                                {leftLists.map
+                                    ((item) => (
+                                        <span key={item} className="py-0.5 px-1 lg:py-1 lg:px-0.2 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]">
+                                            {item}
+                                        </span>
+                                    ))}
+                                <span className="lg:py-4 lg:px-3 py-1 px-3 rounded-lg text-center bg-[#10132e"></span>
+                            </div>
+                            <div className="flex flex-col gap-1 md:gap-1 lg:gap-5">
+                                {rightLists.map
+                                    ((item) => (
+                                        <span key={item} className="py-0.5 px-1 lg:py-1 lg:px-0.2 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]">
+                                            {item}
+                                        </span>
+                                    ))}
+                                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132e"></span>
+                            </div>
+                        </div>
+                    )}
+
+                    {id === 6 && (
+                        <div className="mt-5 relative">
+                            <div className={`absolute -bottom-5 right-0`}>
+                                <Lottie options={defaultOptions} height={200} width={400} />
+                            </div>
+
+                            <MagicButton
+                                title={copied ? "Email is Copied!" : "Copy my email address"}
+                                icon={<IoCopyOutline />}
+                                position="left"
+                                handleClick={handleCopy}
+                                otherClasses="!bg-[#161A31]"
+                            />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ my-portfolio/sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://75466eeb237c62d7f048ee1312aea5d5@o4507539891421184.ingest.us.sentry.io/4507539900530688",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
