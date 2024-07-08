e0a2ede2a21185117bf7a5d331545e629576491a
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-07-08 20:20:37 +0000

    Update 3d-pin.tsx
    Fixed hydration issue

--- my-portfolio/components/ui/3d-pin.tsx
+++ my-portfolio/components/ui/3d-pin.tsx
@@ -29,14 +29,18 @@ export const PinContainer = ({
   };
 
   return (
-    <Link
+    <div
       className={cn(
         "relative group/pin z-50  cursor-pointer",
         containerClassName
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      href={href || "/"}
+      // href={href || "/"}
+      onClick={(event) => {
+        event.preventDefault();
+        window.open("https://" + href, "_blank");
+      }}
     >
       <div
         style={{
@@ -49,13 +53,13 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
+          className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)]  border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
         >
           <div className={cn(" relative z-50 ", className)}>{children}</div>
         </div>
       </div>
       <PinPerspective title={title} href={href} />
-    </Link>
+    </div>
   );
 };
 
