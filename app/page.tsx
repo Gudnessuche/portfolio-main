0f9a9d4ffedaff6d5e4b6eecc01557935ed11253
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-07-22 14:06:39 +0000

    Update page.tsx
    updated the max-w to 8 from 5

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
-      <div className="max-w-5xl w-full">
+      <div className="max-w-8xl w-full">
         <FloatingNav navItems={navItems}
         />
         <Hero />
