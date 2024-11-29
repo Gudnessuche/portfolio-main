4f534ca61bd66750fa69be7a85d86b85f926c573
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-11-29 23:30:01 +0000

    Update Hero.tsx
    Made more NOSTR specific

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,15 +21,15 @@ const Hero = () => {
     <div className='flex justify-center relative my-20 z10`'>
       <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
         <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-          Dynamic Web Magic with Next.js
+          Bitcoin and NOSTR Addict
         </h2>
         <TextGenerateEffect 
         className='text-center text-[40px] md:text-5xl lg:text-6xl'
-        words='Orange Pilling the world through fun applications'
+        words='Orange Pilling the world using fun applications that're FOSS'
         />
 
         <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-          Hi, I&apos;m Everything Satoshi, a Next.js Dev living in cyberspace.
+          Hi, I&apos;m Everything Satoshi, an Open Source Dev living in cyberspace.
         </p>
 
         <a href="#about">
