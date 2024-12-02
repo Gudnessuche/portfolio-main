4cea899db4a58716672be508e3c4c3190a829156
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-12-02 15:07:41 +0000

    Update index.ts
    Put CTV on there

--- data/index.ts
+++ data/index.ts
@@ -72,11 +72,11 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      title: "BIP 119: CheckTemplateVerify Contribution",
+      des: "Merged PR for CTV helping fix minor issues to help explain the need for Covenants.",
       img: "/p1.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      link: "https://github.com/bitcoin/bips/pull/1704",
     },
     {
       id: 2,
