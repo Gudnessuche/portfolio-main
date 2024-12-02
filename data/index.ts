eaef5184fe603e5c458bb72932bd730860276103
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-12-02 15:49:21 +0000

    Update index.ts
    Added Afstr-AI

--- data/index.ts
+++ data/index.ts
@@ -96,11 +96,11 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      title: "Afstr-AI",
+      des: "Recreated the Gemini AI platform and utilized it's API.",
       img: "/p4.svg",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      link: "https://github.com/Gudnessuche/Afstr-AI",
     },
   ];
   
