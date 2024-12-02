c55aa5da8d338c3a19d85b54ca993070e5f6c688
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-12-02 14:57:01 +0000

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building with Freedom technologies(NOSTR, CASHU)",
+      title: "Currently building with Freedom technologies(NOSTR, CASHU, Bitcoin)",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
