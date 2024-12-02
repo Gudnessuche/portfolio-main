1123577caaf6614156762707bbef2ee26eba7680
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-12-02 14:54:13 +0000

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "Prioritization of client collaboration, fostering open communication",
+      title: "Prioritization of Open Source Software Usage",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
