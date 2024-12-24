6d7dca25407d930514e679e71def291c9c612fee
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-12-24 16:32:49 +0000

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -53,8 +53,8 @@ export const BentoGridItem = ({
     spareImg?: string;
 }) => {
 
-    const leftLists = ["Docker", "", "Bitcoin Script", "Typescript"];
-    const rightLists = ["Python", "NextJS", "Rust", ""];
+    const leftLists = ["Docker", "Bitcoin Script", "Typescript"];
+    const rightLists = ["Python", "NextJS", "Rust"];
 
     const [copied, setCopied] = useState(false);
 
